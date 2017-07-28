8cc2613c6d1f1ccb6f0523eb8d02946bbf016026
ice1000 <ice1000kotlin@foxmail.com>
2017-07-28 22:12:55 +0800

    change-main

--- Main.hs
+++ Main.hs
@@ -4,10 +4,10 @@ import Kt2Dart
 import Control.Monad
 
 main :: IO ()
-main = forever $ do
-  putStr "|>"
-  all' <- getContents
-  putStrLn $ case kotlin2Dart all' of
-    (Left  o) -> o
-    (Right o) -> "Syntax error:\n" ++ o
+main = do
+  forever $ do
+    all' <- getLine
+    putStrLn $ case kotlin2Dart all' of
+      (Left  o) -> o
+      (Right o) -> "Syntax error:\n" ++ o
 --
