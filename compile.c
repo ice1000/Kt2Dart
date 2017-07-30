d85764feedf252d0123d71098afc5b2f4998a55f
ice1000 <ice1000kotlin@foxmail.com>
2017-07-30 17:37:53 +0800

    update-c-script

--- compile.c
+++ compile.c
@@ -3,18 +3,17 @@
 #include <memory.h>
 
 int main(const int argc, const char *argv[]) {
-	if (argc <= 1) {
-		printf("Please specify an input file.\n");
-		return 0;
+	if (argc <= 1) printf("Please specify an input file.\n");
+	else {
+		size_t size = 5000 * sizeof(char);
+		char *buffer = (char *) malloc(size);
+		int ret = fread(buffer, 1, size, fopen(argv[1], "r"));
+		char *command = (char *) malloc(size + 10);
+		memset(command, '\0', size);
+		sprintf(command, "echo \"%s\" | ./Main", buffer);
+		ret = system(command);
+		free(buffer);
+		free(command);
+		return ret;
 	}
-	size_t size = 5000 * sizeof(char);
-	char *buffer = (char *) malloc(size);
-	int ret = fread(buffer, 1, size, fopen(argv[1], "r"));
-	char *command = (char *) malloc(size + 10);
-	memset(command, '\0', size);
-	sprintf(command, "echo \"%s\" | ./Main", buffer);
-	ret = system(command);
-	free(buffer);
-	free(command);
-	return ret;
 }
