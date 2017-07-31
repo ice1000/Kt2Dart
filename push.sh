90fd7e427c20b685cf18150bfecd64b1f608ec79
ice1000 <ice1000kotlin@foxmail.com>
2017-07-31 18:14:41 +0800

    update-shell-script

--- push.sh
+++ push.sh
@@ -1,35 +1,15 @@
 git status
-
 gcc -Wall compile.c -o transpiler -O3
-
 cd ./src/
 ghc Main.hs -O2
-rm *~
-rm *.hi
-rm *.hi-boot
-rm *.o-boot
-rm *.o
+rm *~ *.hi *.hi-boot *.o-boot *.o
 mv ./Main ./../Main
 cd ./../
-
-# rm ./Main
-# rm ./transpiler
-rm *~
-
-echo ""
+rm *~ # ./Main ./transpiler
 read -p "Enter commit message:"
-
 git add *
 git stage *
 git commit -a -m "${REPLY}"
-
 git status
-
-echo ""
-echo "commit finished..."
-
 git gc
 git push origin master
-
-echo ""
-echo "hia hia I have finished!"
