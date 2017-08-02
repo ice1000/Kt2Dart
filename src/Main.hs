7ea7e17f6605d4a3fbe4af23cb3ad823e57150ad
ice1000 <ice1000kotlin@foxmail.com>
2017-08-03 02:01:46 +0800

    update some files

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,20 @@ module Main where
 import Kt2Dart
 import Control.Monad
 
+import System.IO
+
+doRead fn op = do
+  h <- openFile fn ReadMode
+  op <$> hGetContents h
+  hClose h
+--
+
 main :: IO ()
 main = do
-  allCodes <- getContents
-  putStrLn allCodes
-  putStrLn $ case kotlin2Dart allCodes of
-    (Left  o) -> o
-    (Right o) -> o
+  putStrLn "Please specify the input file:"
+  file <- getLine
+  doRead file $ \allCodes ->
+    putStrLn $ case kotlin2Dart allCodes of
+      (Left  o) -> o
+      (Right o) -> o
 --
